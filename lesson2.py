ebf14129caaf0a357e831b260dfd02654ff1236e
Leeroy133 <nikita.kozytenko@gmail.com>
2024-09-30 19:43:35 +0300

    Update lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -6,19 +6,21 @@ from dotenv import load
 load()
 
 website = 'https://dvmn.org/referrals/i7iQz20ghmw3Lig1dUOk0B1kzJwSktCx6f2Swt8Z/'
-my_name = 'Никита'
-friend_name = "Михаил"
-mail_from = 'laraxsmurf@yandex.ru'
-mail_to = 'nikita.kozytenko@yandex.ru'
+sender_name = 'Никита'
+recipient_name = "Михаил"
+sender_email = 'laraxsmurf@yandex.ru'
+recipient_email = 'nikita.kozytenko@yandex.ru'
 subject = 'Приглашение!'
 content_type = 'text/plain; charset=''"''UTF-8''"'
 
 letter = """\
-From: {mail_from}
-To: {mail_to}
+From: {sender_email}
+To: {recipient_email}
 Subject: {subject}
 Content-Type: {content_type};
-Привет, {friend_name}! {my_name} приглашает тебя на сайт {website}!
+
+
+Привет, {recipient_name}! {sender_name} приглашает тебя на сайт {website}!
 
 {website} — это новая версия онлайн-курса по программированию. 
 Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя. 
@@ -33,13 +35,11 @@ Content-Type: {content_type};
 Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
 
 Регистрируйся → {website}  
-На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.""" .format(website=website, my_name=my_name, friend_name=friend_name, mail_from=mail_from, mail_to=mail_to, subject=subject, content_type=content_type)
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.""" .format(website=website, sender_name=sender_name, recipient_name=recipient_name, sender_email=sender_email, recipient_email=recipient_email, subject=subject, content_type=content_type)
 
 letter = letter.encode("UTF-8")
 
-print(letter)
-
 server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
 server.login(os.getenv('LOGIN'), os.getenv('PASSWORD'))
-server.sendmail('laraxsmurf@yandex.ru', 'nikita.kozytenko@yandex.ru', letter)
+server.sendmail(sender_email, recipient_email, letter)
 server.quit()
